bf52feb933cb0ad72100b0110f6d9bc7a5a5c9a1
emmfatsneh@gmail.com <emmfatsneh@gmail.com>
2024-09-15 16:23:47 +0100

    Hrapple

--- employees/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from .models import Employee
-from .serializers import EmployeeSerializer
-
-# Create your views here.
-
-class EmployeeViewSet(viewsets.ModelViewSet):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
